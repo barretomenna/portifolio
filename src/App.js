104069ef50e6cdd2c971c7cee5028ec2d39d99db
Rafael barreto <rsmennabarreto@gmail.com>
2019-04-18 14:09:04 -0300

    iniciado criação da navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import NavBar from './components/navbar/navbar';
 import './App.css';
+import '../node_modules/nes.css/css/nes.min.css';
 
 class App extends Component {
   render() {
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,12 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './navbar.scss';
 
 export default class NavBar extends Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="navbarContainer">
-                oi
+                <section>
+                    <i class="nes-ash"></i>
+                </section>
+
+                <nav className="navbar">
+                    <a>skills</a>
+                    <a>Sobre</a>
+                </nav>
+
+                <section>
+                    <i class="nes-charmander"></i>
+                </section>
             </div>
         );
     }
