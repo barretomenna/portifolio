e7acd1cac0d3f6ddb5e6cbf8f9d9b9f452ec425c
Rafael barreto <rsmennabarreto@gmail.com>
2019-04-18 08:36:03 -0300

    first commit

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './navbar.scss';
+
+export default class NavBar extends Component {
+
+    render(){
+        return(
+            <div className="navbarContainer">
+                oi
+            </div>
+        );
+    }
+}
