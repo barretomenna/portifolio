bb04b35df2d7ac5e52669e76b4c20b5468237a87
Rafael Barreto <rafael.barreto@cwi.com.br>
2019-05-20 21:35:42 -0300

    Updates

---
+++ precache-manifest.2783e82efe024c9205afe9aa9fad7785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91a74ff0904045e04265",
+    "url": "/portifolio/static/js/runtime~main.e9cc7322.js"
+  },
+  {
+    "revision": "03eb8b24c0cc6140d601",
+    "url": "/portifolio/static/js/main.e0f14740.chunk.js"
+  },
+  {
+    "revision": "7da4ee9f6e818239096d",
+    "url": "/portifolio/static/js/2.37475b2e.chunk.js"
+  },
+  {
+    "revision": "03eb8b24c0cc6140d601",
+    "url": "/portifolio/static/css/main.c75a07ad.chunk.css"
+  },
+  {
+    "revision": "7da4ee9f6e818239096d",
+    "url": "/portifolio/static/css/2.3b8a1980.chunk.css"
+  },
+  {
+    "revision": "c8fc5e8da30f68be356ca1cf52d1bed7",
+    "url": "/portifolio/index.html"
+  }
+];
